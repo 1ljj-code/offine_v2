8fe6ff5d60e420da239d739ee12daa33d200eda2
lenovo <1628170569@qq.com>
2025-09-23 17:24:43 +0800

    addr

---
+++ offline_sql/gmall/test.sql
@@ -0,0 +1,5 @@
+-- auto-generated definition
+drop database if exists bigdata_offline_v1_ws cascade ;
+create schema bigdata_offline_v1_ws
+    location 'hdfs://cdh01:8020/user/hive/warehouse/bigdata_offline_v1_ws.db';
+
